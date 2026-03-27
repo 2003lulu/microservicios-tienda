35d4383f83d804e5470a9c940246c9c831bbef68
alvaro <inmortalpach@gmail.com>
2026-03-27 13:18:58 -0600

    Actualización de java y resources en usuario-service

--- usuario-service/src/main/java/com/tienda/usuario/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/tienda/usuario/UsuarioServiceApplication.java
@@ -2,12 +2,11 @@ package com.tienda.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UsuarioServiceApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(UsuarioServiceApplication.class, args);
     }
 }
